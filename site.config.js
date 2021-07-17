29945acef351984e8578fb838526798e3d1d45d4
Mattia1010 <87578145+Mattia1010@users.noreply.github.com>
2021-07-17 18:17:45 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Evenstudio',
+  domain: 'evenstudio.demo.it',
+  author: 'Evenstudio',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Architettura e design',
+  socialImageTitle: 'Evenstudio',
+  socialImageSubtitle: 'Hello',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
